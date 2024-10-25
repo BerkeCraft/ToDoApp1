a753a409aec828bbf02614cdd0e74c7f999067e2
BerkeCraft <berkecora@hotmail.com>
2024-10-25 20:04:48 +0300

    Initial Commit

---
+++ ToDoAppCora/ToDoAppCoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppCoraApp.swift
+//  ToDoAppCora
+//
+//  Created by Berke Cora on 25.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppCoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
